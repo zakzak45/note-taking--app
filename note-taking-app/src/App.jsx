51eed29ba3c30f5769b7fe725c3398308f876d18
zakzak45 <147825634+zakzak45@users.noreply.github.com>
2025-04-16 22:06:36 +0200

    first commit

---
+++ note-taking-app/src/App.jsx
@@ -0,0 +1,13 @@
+import "./App.css";
+import Navbar from "./navbar";
+function App() {
+  return (
+    <>
+      <Navbar />
+      <p>navbar</p>
+      <h1>hell</h1>
+    </>
+  );
+}
+
+export default App;
